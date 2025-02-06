7938045db04eb7c40ac35d502d61a791174b27cc
SpBarber <66115730+SantipBarber@users.noreply.github.com>
2025-02-06 17:38:42 +0100

    complete

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 
 
@@ -6,17 +6,34 @@ const blogCollection = defineCollection({
     'type': 'content',
     'schema': ({image}) => 
         z.object({
-        'title': z.string(),
-        'date':  z.date(),
-        'description': z.string(),
-        'image': image(),
+        title: z.string(),
+        date:  z.date(),
+        description: z.string(),
+        image: image(),
         // Relación
-        'author':z.string(),
+        // author: z.string(),
+        author: reference('author'),
         // Relación
-        'tags':  z.array(z.string())
+        tags:  z.array(z.string()),
+        isDraft: z.boolean().default(false)
     })
 })
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({image}) => 
+        z.object({
+            name: z.string(),
+            avatar: image(),
+            twitter: z.string(),
+            linkedIn: z.string(),
+            github: z.string(),
+            bio: z.string(),
+            subtitle: z.string()
+        })
+})
+
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection
 }
